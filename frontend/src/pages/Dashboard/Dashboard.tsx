f657d2f38621dfbfdde8fef8d6b7e8cb4b3bd529
Blem2011 <Krenshoesgod@gmail.com>
2026-06-11 23:41:47 -0300

    Importar axios en dashboard para activar campanita

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Usuarios from './sub-modules/Usuarios';
 import Clientes from './sub-modules/Clientes';
 import Proveedores from './sub-modules/Proveedores';
